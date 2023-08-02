380480d7e82604c317e25b0c8b2ce0bbb45e61ae
medinalicina <medinalicina94@gmail.com>
2023-08-02 11:34:33 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const customStyle ={
+    color:"pink",
+    fontSize: "23px",
+  }
+  const onClickHandler= () => console.log("MEDINA")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={customStyle} onClick={onClick}></h1>
+    <button onClick={() => console.log("kliknula")}> Klikni</button>
     </div>
+     
   );
 }
 
