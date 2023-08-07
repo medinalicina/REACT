d0437467b382c02304b97de74cdbb6ebf58abbd5
medinalicina <medinalicina94@gmail.com>
2023-08-07 21:50:38 +0200

    vezba

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import React, { useState } from 'react';
 const App = () => {
   const[broj, setBroj] = useState(0);
   return (
-    <div className='app'>
-      <button onClick={()=> setBroj(broj+1)}>DODAJ</button>
-      {broj}
-      <button onClick={()=> setBroj(broj-1)}>SMANJI</button>
-    </div>
-    // <div className="app">
-    //   <h1>Dobrodošli u prodavnicu patika</h1>
-    //   <Card
-    //     image={''}
-    //     title="Patike Jordan"
-    //     description="Ovo su neverovatne patike koje ćete obožavati!"
-    //     price={20000}
-    //   />
-    //   <Button price={20000} />
+    // <div className='app'>
+    //   <button onClick={()=> setBroj(broj+1)}>DODAJ</button>
+    //   {broj}
+    //   <button onClick={()=> setBroj(broj-1)}>SMANJI</button>
     // </div>
+    <div className="app">
+      <h1>Dobrodošli u prodavnicu patika</h1>
+      <Card
+        image={''}
+        title="Patike Jordan"
+        description="Ovo su neverovatne patike koje ćete obožavati!"
+        price={20000}
+      />
+      <Button price={20000} />
+    </div>
   );
 };
 
