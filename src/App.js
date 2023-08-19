04e9b0c9c80b706ff939a1eda644925f69588844
medinalicina <medinalicina94@gmail.com>
2023-08-20 00:21:48 +0200

    STA

--- src/App.js
+++ src/App.js
@@ -18,14 +18,7 @@ function App() {
 
 
 <img
-className="avatar"
-src="https://i.imgur.com/1bX5QH6.jpg"
-alt="Lin Lanying"
-width={100}
-height={100}
-/>
-);
-}
+
 
 export default function Profile() {
 return (
