b390c4ffc5d6eb0a1c0fe01e7b8c7c8693527899
medinalicina <medinalicina94@gmail.com>
2023-08-20 00:23:21 +0200

    nis

--- src/App.js
+++ src/App.js
@@ -18,6 +18,37 @@ function App() {
 
 
 <img
+  <img
+  className="avatar"
+  src="https://i.imgur.com/1bX5QH6.jpg"
+  alt="Lin Lanying"
+  width={100}
+  height={100}
+/>
+);
+}
+
+export default function Profile() {
+return (
+<Avatar />
+);
+}
+
+<img
+className="avatar"
+src="https://i.imgur.com/1bX5QH6.jpg"
+alt="Lin Lanying"
+width={100}
+height={100}
+/>
+);
+}
+
+export default function Profile() {
+return (
+<Avatar />
+);
+}
 
 
 export default function Profile() {
