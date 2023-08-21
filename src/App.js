841f425b1fdebbe320e01f8ac753f1b952a89f98
medinalicina <medinalicina94@gmail.com>
2023-08-21 22:44:26 +0200

    vezbanje

--- src/App.js
+++ src/App.js
@@ -48,33 +48,6 @@ export default function Profile() {
 }
 
 
-export default function Profile() {
-  return (
-    <div>
-      <Avatar
-        size={100}
-        person={{ 
-          name: 'Katsuko Saruhashi', 
-          imageId: 'YfeOqp2'
-        }}
-      />
-      <Avatar
-        size={80}
-        person={{
-          name: 'Aklilu Lemma', 
-          imageId: 'OKS67lh'
-        }}
-      />
-      <Avatar
-        size={50}
-        person={{ 
-          name: 'Lin Lanying',
-          imageId: '1bX5QH6'
-        }}
-      />
-    </div>
-  );
-}
 
 
 // function App() {
