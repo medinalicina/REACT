e0b79f0bbafc9b341c21aad73e9ade1904558047
medinalicina <medinalicina94@gmail.com>
2023-08-22 15:26:26 +0200

    vezba

--- src/App.js
+++ src/App.js
@@ -186,3 +186,15 @@ export default function Profile() {
 //       <Button price={20000} />
 //     </div>
 //   );
+export default function TodoList() {
+  return (
+    <ul style={{
+      backgroundColor: 'black',
+      color: 'pink'
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
+  );
+}
