36e7fc71dc3555f29a0d93712814361bda7334de
medinalicina <medinalicina94@gmail.com>
2023-08-22 15:25:07 +0200

    nis

--- src/App.js
+++ src/App.js
@@ -186,16 +186,3 @@ export default function Profile() {
 //       <Button price={20000} />
 //     </div>
 //   );
-
-export default function TodoList() {
-  return (
-    <ul style={{
-      backgroundColor: 'black',
-      color: 'pink'
-    }}>
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
-  );
-}
