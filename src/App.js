77f35014aa133fe52dc77e160e232494f08781f2
medinalicina <medinalicina94@gmail.com>
2023-08-23 15:48:36 +0200

    vezb

--- src/App.js
+++ src/App.js
@@ -198,3 +198,19 @@ export default function TodoList() {
     </ul>
   );
 }
+
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
+  return (
+    <h1>To Do List for {formatDate(today)}</h1>
+  );
+}
