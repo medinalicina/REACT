1f51e0a62e34308adcd2b98c506cf2d7e4135394
medinalicina <medinalicina94@gmail.com>
2023-08-26 23:06:22 +0200

    nis

--- src/App.js
+++ src/App.js
@@ -218,28 +218,3 @@ export default function TodoList() {
 
 
 
-
-
-
-
-const today = new Date();
-
-function formatDate(date) {
-  return new Intl.DateTimeFormat(
-    'en-US',
-    { weekday: 'long' }
-  ).format(date);
-}
-
-export default function TodoList() {
-  return (
-    <h1>To Do List for {formatDate(today)}</h1>
-  );
-}
-
-
-
-const today = new Date();
-
-
-
